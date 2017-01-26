e013ca5fa75816ac8857999c3b661b577d8cf99a
Sergio Hilgert <hilgertfacul@gmail.com>
2017-01-26 23:43:55 +0000

    Fix create, add flash and errors messages, create show

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get '/about' => 'pages#about'
+  
+  resources :articles
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
