e971847e8057d326ea2f2787002068f32e276244
Sergio Hilgert <hilgertfacul@gmail.com>
2017-01-28 13:58:22 +0000

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ActiveRecord::Base
             length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
+            
+  has_secure_password
 end
---
+++ db/migrate/20170128135346_add_passwords_to_user.rb
@@ -0,0 +1,5 @@
+class AddPasswordsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
