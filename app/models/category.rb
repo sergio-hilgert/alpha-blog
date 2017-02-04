a61ceba599a433eeacda85a6469becdb984fc512
Sergio Hilgert <hilgertfacul@gmail.com>
2017-02-04 21:27:43 +0000

    Add category and article association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
 end
