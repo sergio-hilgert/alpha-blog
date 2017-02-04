32dfdbfea4251863feb21926ca697401b00aeed4
Sergio Hilgert <hilgertfacul@gmail.com>
2017-02-04 22:22:51 +0000

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
     end
     
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
     def require_same_user
       if current_user != @article.user and !current_user.admin?
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  
   has_many :article_categories
   has_many :articles, through: :article_categories
-  validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
 end
